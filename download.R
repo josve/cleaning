cca621e316fb7d05aca0583bc0c2505781ec77bf
Jonas Svensson <jonas.m.svensson@gmail.com>
2015-06-15 21:16:42 +0200

    initial version of scrips

---
+++ download.R
@@ -0,0 +1,28 @@
+# This script downloads the source data for the cleaning assignment,
+# into the data directory
+if (!file.exists("data"))
+{
+    dir.create("data")
+}
+
+# Keep track of when the data was downloaded.
+dateDownloaded <- date()
+dateFile <- file("data/downloadedDate.txt")
+writeLines(as.character(dateDownloaded), dateFile)
+close(dateFile)
+
+# Download the dataset
+
+zippedDataSetPath <- "data/UCI_HAR_Dataset.zip"
+download.file(
+    url = "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip", 
+    destfile = zippedDataSetPath,
+    quiet = FALSE,
+    method="curl")
+
+# Unzip the zip
+unzip(
+    zipfile = zippedDataSetPath,
+    overwrite = TRUE,
+    exdir = "data")
+
---
+++ run_analysis.R
@@ -0,0 +1,234 @@
+library("dplyr")
+
+# First validate that all paths and files exist
+dataDir <- "."
+
+if (!file.exists(dataDir))
+{
+    stop("data directory does not exist.")
+}
+
+dataSetDir <- paste(dataDir, "UCI HAR Dataset", sep="/")
+
+if (!file.exists(dataSetDir))
+{
+    stop("dataset directory does not exist.")
+}
+
+trainDir <- paste(dataSetDir, "train", sep="/")
+
+if (!file.exists(trainDir))
+{
+    stop("training directory does not exist.")
+}
+
+testDir <- paste(dataSetDir, "test", sep="/")
+
+if (!file.exists(testDir))
+{
+    stop("test directory does not exist.")
+}
+
+trainDir <- paste(dataSetDir, "train", sep="/")
+
+if (!file.exists(trainDir))
+{
+    stop("training directory does not exist.")
+}
+
+# Load the data set and assign column names.
+
+### activity_labels
+activityLabelsPath <- paste(dataSetDir, "activity_labels.txt", sep="/")
+if (!file.exists(activityLabelsPath))
+{
+    stop("activity labels file does not exist.")
+}
+
+activityLabelsDF <- 
+    read.delim(
+         activityLabelsPath, 
+         header=FALSE, 
+         sep = " ", 
+         colClasses=c("numeric", "character"),
+         stringsAsFactors = FALSE)
+
+activityLabelsDF <- tbl_df(activityLabelsDF)
+
+names(activityLabelsDF) <- c("id", "activity")
+
+### features
+featuresPath <- paste(dataSetDir, "features.txt", sep="/")
+if (!file.exists(featuresPath))
+{
+    stop("features file does not exist.")
+}
+
+featuresDF <- 
+    read.delim(
+        featuresPath, 
+        header=FALSE, 
+        sep = " ", 
+        colClasses=c("numeric", "character"),
+        stringsAsFactors = FALSE)
+
+featuresDF <- tbl_df(featuresDF)
+
+names(featuresDF) <- c("id", "name")
+
+### X_train
+# 561 variables
+xTrainPath <- paste(trainDir, "X_train.txt", sep="/")
+if (!file.exists(xTrainPath))
+{
+    stop(paste(xTrainPath,"file does not exist."))
+}
+
+XtrainDF <- 
+    read.table(
+        xTrainPath, 
+        header=FALSE, 
+        sep = "", 
+        colClasses="numeric",
+        stringsAsFactors = FALSE)
+
+XtrainDF <- tbl_df(XtrainDF)
+names(XtrainDF) <- featuresDF$name
+mean_names <- grepl("mean()", names(XtrainDF), fixed=TRUE)
+std_names <- grepl("std()", names(XtrainDF), fixed=TRUE)
+filtered_names <- mean_names | std_names
+XtrainDF <- XtrainDF[,filtered_names]
+
+### y_train
+yTrainPath <- paste(trainDir, "y_train.txt", sep="/")
+if (!file.exists(yTrainPath))
+{
+    stop(paste(yTrainPath,"file does not exist."))
+}
+
+YtrainDF <- 
+    read.table(
+        yTrainPath, 
+        header=FALSE, 
+        sep = "", 
+        colClasses="numeric",
+        stringsAsFactors = FALSE)
+
+YtrainDF <- tbl_df(YtrainDF)
+names(YtrainDF) <- c("label")
+
+# subject_train.txt
+subjectTrainPath <- paste(trainDir, "subject_train.txt", sep="/")
+if (!file.exists(subjectTrainPath))
+{
+    stop(paste(subjectTrainPath,"file does not exist."))
+}
+
+subjectTrainDF <- 
+    read.table(
+        subjectTrainPath, 
+        header=FALSE, 
+        sep = "", 
+        colClasses="numeric",
+        stringsAsFactors = FALSE)
+
+subjectTrainDF <- tbl_df(subjectTrainDF)
+names(subjectTrainDF) <- c("subject")
+
+# Append all cols to a train dataset.
+train <- XtrainDF
+train$label <- YtrainDF$label
+train$subject <- subjectTrainDF$subject
+
+# join in the activity label
+train <- merge(train, activityLabelsDF, by.x="label", by.y="id")
+train <- tbl_df(train)
+
+### test
+### X_test
+# 561 variables
+xTestPath <- paste(testDir, "X_test.txt", sep="/")
+if (!file.exists(xTestPath))
+{
+    stop(paste(xTestPath,"file does not exist."))
+}
+
+XtestDF <- 
+    read.table(
+        xTestPath, 
+        header=FALSE, 
+        sep = "", 
+        colClasses="numeric",
+        stringsAsFactors = FALSE)
+
+XtestDF <- tbl_df(XtestDF)
+names(XtestDF) <- featuresDF$name
+mean_names <- grepl("mean()", names(XtestDF), fixed=TRUE)
+std_names <- grepl("std()", names(XtestDF), fixed=TRUE)
+filtered_names <- mean_names | std_names
+XtestDF <- XtestDF[,filtered_names]
+
+
+### y_test
+yTestPath <- paste(testDir, "y_test.txt", sep="/")
+if (!file.exists(yTestPath))
+{
+    stop(paste(yTestPath,"file does not exist."))
+}
+
+YtestDF <- 
+    read.table(
+        yTestPath, 
+        header=FALSE, 
+        sep = "", 
+        colClasses="numeric",
+        stringsAsFactors = FALSE)
+
+YtestDF <- tbl_df(YtestDF)
+names(YtestDF) <- c("label")
+
+# subject_test.txt
+subjectTestPath <- paste(testDir, "subject_test.txt", sep="/")
+if (!file.exists(subjectTestPath))
+{
+    stop(paste(subjectTestPath,"file does not exist."))
+}
+
+subjectTestDF <- 
+    read.table(
+        subjectTestPath, 
+        header=FALSE, 
+        sep = "", 
+        colClasses="numeric",
+        stringsAsFactors = FALSE)
+
+subjectTestDF <- tbl_df(subjectTestDF)
+names(subjectTestDF) <- c("subject")
+
+# Append all cols to a test dataset.
+test <- XtestDF
+test$label <- YtestDF$label
+test$subject <- subjectTestDF$subject
+
+# join in the activity label
+test <- merge(test, activityLabelsDF, by.x="label", by.y="id")
+test <- tbl_df(test)
+
+test$origin <- "test"
+train$origin <- "training"
+combined <- rbind(test, train)
+combined <- tbl_df(combined)
+combined$label <- NULL
+names(combined) <- gsub("()", "", names(combined), fixed=TRUE)
+names(combined) <- gsub("-mean", "Mean", names(combined), fixed=TRUE)
+names(combined) <- gsub("-std", "StandardDeviation", names(combined), fixed=TRUE)
+names(combined) <- gsub("-", "", names(combined), perl=TRUE)
+
+grouped_combined <- 
+    combined %>% 
+    select (-origin) %>% 
+    group_by(subject, activity)
+
+result <- grouped_combined %>% summarise_each(funs(mean))
+
+write.table(result, file="tidy.txt", row.name=FALSE)
